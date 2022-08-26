e6b33a0ed1d72447e0bd2dad791c9992aabf8bfc
biancaantunes98 <bianca.antunes98@gmail.com>
2022-08-25 23:24:01 -0300

    integrating firebase

---
+++ assets/js/firebase/app.js
@@ -0,0 +1,19 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAJhiJQEdJX-VoG0pG8KfzbvgyThSGvwIM",
+    authDomain: "upside-down-world-d0c40.firebaseapp.com",
+    projectId: "upside-down-world-d0c40",
+    storageBucket: "upside-down-world-d0c40.appspot.com",
+    messagingSenderId: "937900428533",
+    appId: "1:937900428533:web:bfb26fb00f8a0b1460c9bc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+export default app
---
+++ assets/js/firebase/hellfire-club.js
@@ -0,0 +1,6 @@
+import app from "./firebase/app.js"
+import { } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-firebase.js'
+
+export function subscribeTohellfireClub(subscription){
+    console.log('subscription', subscription)
+}
