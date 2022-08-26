a30a1d0872ad96ad5c59f8f22ec956024f1cb2f1
biancaantunes98 <bianca.antunes98@gmail.com>
2022-08-26 17:09:09 -0300

    adding email and input validation

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@ const txtCharacter = document.getElementById('txtCharacter')
 
 const btnSubscribe = document.getElementById('btnSubscribe')
 
-btnSubscribe.addEventListener('click', () => {
+btnSubscribe.addEventListener('click', async () => {
     const subscription = {
         name: txtName.value,
         email: txtEmail.value,
@@ -17,5 +17,40 @@ btnSubscribe.addEventListener('click', () => {
         character: txtCharacter.value
     }
 
-    subscribeTohellfireClub(subscription)
+    const isInputsFilled = IsInputsFilled(subscription)
+    const isEmail = IsEmail(subscription.email)
+
+    if (isInputsFilled && isEmail) {
+        const subscriptionId = await subscribeTohellfireClub(subscription)
+        console.log('Inscrito com sucesso: ' + subscriptionId)
+    } else {
+        alert('Digite um email válido')
+    }
 })
+
+function IsEmail(email) {
+    var validRegex = new RegExp(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)
+
+    if (validRegex.test(email)) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function IsInputsFilled(subscription) {
+    if (subscription.name == '')
+        return false
+
+    if (subscription.email == '')
+        return false
+
+    if (subscription.level == '')
+        return false
+
+    if (subscription.character == '')
+        return false
+
+    return true
+}
+
