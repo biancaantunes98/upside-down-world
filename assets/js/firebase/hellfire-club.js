818c8178f12f9048480037f26e328162524276c1
biancaantunes98 <bianca.antunes98@gmail.com>
2022-08-26 22:41:15 -0300

    creating form validations

--- assets/js/firebase/hellfire-club.js
+++ assets/js/firebase/hellfire-club.js
@@ -2,7 +2,6 @@ import app from "./app.js"
 import { getFirestore, collection, addDoc } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-firestore.js'
 
 export async function subscribeTohellfireClub(subscription) {
-    debugger
     const db = getFirestore(app)
     const hellfireClubCollection = collection(db, 'hellfire-club')
     const docRef = await addDoc(hellfireClubCollection, subscription)
